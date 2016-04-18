6ed7ef8ef78e0cad11216c1b38f6fd5df0ff4f07
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-17 21:13:13 -0400

    Add M permissions logic

--- app/src/main/java/com/example/runtimepermissions/MainActivity.java
+++ app/src/main/java/com/example/runtimepermissions/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.runtimepermissions;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    private static final int CODE_CALL_LOG = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CALL_LOG)) {
+                Log.d(TAG, "onCreate: " + "Show explanation");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, CODE_CALL_LOG);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, CODE_CALL_LOG);
+            }
+        } else {
+            Log.d(TAG, "onCreate: " + "Permission already granted!");
+            printCallLog();
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CODE_CALL_LOG: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: Good to go!");
+                    printCallLog();
+                } else {
+                    Log.d(TAG, "onRequestPermissionsResult: Bad user");
+                }
+            }
+        }
+    }
+
+    private void printCallLog() {
+        Log.d(TAG, "printCallLog: ");
+    }
+
 }
